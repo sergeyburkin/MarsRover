a01e8a6efa60f3cb64b62ac1ba8623cb01d202e1
Bopobywek <domiantor@yandex.ru>
2021-01-24 17:13:05 +0300

    added db, standard maps

--- Backend/app.py
+++ Backend/app.py
@@ -1,21 +1,22 @@
 import json
+import pickle
 
-from flask import Flask, request, json, render_template
+from flask import Flask, request, json, render_template, abort
 
-# from db import db
+from storage import db, Map, add_to_db
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///origami.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mars_rover.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://PaperFoldingSkil:password@PaperFoldingSkill.mysql.pythonanywhere-services.com/PaperFoldingSkil$skill"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
 # app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
-# db.app = app
-# db.init_app(app=app)
-# try:
-#     db.create_all()
-# except Exception as e:
-#     pass
+db.app = app
+db.init_app(app=app)
+try:
+    db.create_all()
+except Exception as e:
+    pass
 
 
 @app.route("/")
@@ -30,12 +31,17 @@ def health():
 
 @app.route("/srvc/map/")
 def all_maps():
-    return "all maps"
+    maps = Map.query.all()
+    maps = [pickle.loads(map_.map_data) for map_ in maps]
+    return json.dumps({"maps": maps})
 
 
 @app.route("/srvc/map/<int:map_id>")
 def get_map(map_id):
-    return json.dumps({"map_id": map_id})
+    map_ = Map.query.filter_by(id=map_id).first()
+    if not map_:
+        abort(400)
+    return json.dumps(pickle.loads(map_.map_data))
 
 
 @app.route("/srvc/path/")
@@ -53,11 +59,31 @@ def cost_by_map_id():
     return "ok"
 
 
-@app.route("/srvc/path/cost_map")
+@app.route("/srvc/path/cost_map", methods=["POST"])
 def cost_by_map():
     return "ok"
 
 
-@app.route("/srvc/map/upload")
-def upload_map():
+@app.route("/srvc/map/upload/<int:map_id>", methods=["POST"])
+def upload_map(map_id):
+    json_file = request.json
+    maps = Map.query.filter_by(id=map_id).all()
+    if maps:
+        abort(400)
+        return "Alredy exist"
+    new_map = Map()
+    new_map.map_data = pickle.dumps(json_file)
+    add_to_db(new_map)
+    return "ok"
+
+
+@app.route("/srvc/map/change/<int:map_id>", methods=["POST"])
+def change_map(map_id):
+    json_file = request.json
+    map_ = Map.query.filter_by(id=map_id).first()
+    if not map_:
+        abort(400)
+        return "Alredy exist"
+    map_.map_data = pickle.dumps(json_file)
+    add_to_db(map_)
     return "ok"
--- Backend/storage.py
+++ Backend/storage.py
@@ -15,3 +15,8 @@ DAY_FOR_RESET = 2
 class Map(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     map_data = db.Column(db.PickleType)
+
+
+def add_to_db(obj):
+    db.session.add(obj)
+    db.session.commit()
